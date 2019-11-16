0c601e3ace7b7ff60c1c400be33e3ef09f734428
Enock Maregere <Enock.Maregere@eoh.com>
2019-11-16 17:23:55 +0200

    added class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { StudentComponent } from './student/student.component';
 import { TeacherComponent } from './teacher/teacher.component';
+import { ClassComponent } from './class/class.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StudentComponent,
-    TeacherComponent
+    TeacherComponent,
+    ClassComponent
   ],
   imports: [
     BrowserModule
