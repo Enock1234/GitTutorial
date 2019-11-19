ab7c45008c9af27c676b435bd4b99e1756d97daf
Enock Maregere <Enock.Maregere@eoh.com>
2019-11-19 19:11:36 +0200

    changed file

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from 'selenium-webdriver/http';
 
+
 @Component({
   selector: 'app-student',
   templateUrl: './student.component.html',
@@ -8,7 +9,7 @@ import { HttpClient } from 'selenium-webdriver/http';
 })
 export class StudentComponent implements OnInit {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit() {
   }
